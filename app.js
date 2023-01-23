20e80b1997b9880a89032da65d569d81fbc3f59a
Vinicius Augusto <vini.a1706@gmail.com>
2023-01-23 20:57:49 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
+console.log ("Aplicação etsá se conectando ao banco de dados")
+// Mais um console log de teste:
+console.log ("Commit sem ver")
+console.log ("Enviando para o Git Hub")
