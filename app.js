5467d1bd034b4a2079181f59db019d1174756b94
Vinicius Augusto <vini.a1706@gmail.com>
2023-01-23 21:08:58 -0300

    Voltando a versão

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log ("Rodando o sistema de gerenciamento de jogos")
-console.log ("Aplicação etsá se conectando ao banco de dados")
-// Mais um console log de teste:
-console.log ("Commit sem ver")
-console.log ("Enviando para o Git Hub")
